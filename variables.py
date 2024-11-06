a72716ac5d1a4550b0b75f577a85ae55136a0ffe
JoseB10 <barrosjose1004@gmail.com>
2024-11-05 22:35:54 -0500

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,14 @@
+#perfil="no"
+#sexo="f"
+#ciudad="barranquilla"
+#entrevista="si"
+#print(perfil=="si" and sexo=="f" and ciudad=="barranquilla" and entrevista=="si")
+#print(True and True and True and True)
+#print(True or False and not True)
+#print("hola",end="*")
+#print("Jose", "Bienvenido", "",sep="*")
+producto="shampoo"
+precio=45000
+cantidad=int(input("Por favor digite cuantos "+producto+" va llevar: "))
+total=precio*cantidad
+print(f"el costo total es: {total}")
